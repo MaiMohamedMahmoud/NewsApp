b91862ba9a8b9fa5c6c0fdd4cc6591541692a4b5
MaiMohamedMahmoud <mai.mohamed.mahmoud91@gmail.com>
2021-07-26 20:46:12 +0200

    Add Resource sealed class

--- app/src/main/java/com/codinginflow/mvvmnewsapp/utlis/NetworkBoundResource.kt
+++ app/src/main/java/com/codinginflow/mvvmnewsapp/utlis/NetworkBoundResource.kt
@@ -1,12 +1,26 @@
 package com.codinginflow.mvvmnewsapp.utlis
 
-import com.codinginflow.mvvmnewsapp.domain.model.Article
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 
+//Request is the type of response Api
+//Result is the type using in UI.
 fun <Result, Request> performGetOperation(
     databaseQuery: () -> Flow<Result>,
     networkCall: suspend () -> Request,
     saveCallResult: suspend (Request) -> Unit
-){
+):Flow<Result> = channelFlow {
+    val databaseResult = databaseQuery().first()
+    send(databaseResult)
+    try{
+        saveCallResult(networkCall())
+    }catch (error:Exception){
+
+    }
+
+
+
 
 }
